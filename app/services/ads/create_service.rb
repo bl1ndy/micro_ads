5d9161be0b014320df82243b46cb1c110ed9d395
bl1ndy <ayoblindstone@gmail.com>
2023-06-17 22:57:06 +0300

    feat: add ads geocoding

--- app/services/ads/create_service.rb
+++ app/services/ads/create_service.rb
@@ -17,12 +17,21 @@ module Ads
     def call
       @ad = ::Ad.new(@ad.to_h)
       @ad.user_id = @user_id
+      @ad.lat = coordinates&.first
+      @ad.lon = coordinates&.last
 
       if @ad.valid?
         @ad.save_changes
       else
         fail!(@ad.errors)
       end
+    end
+
+    private
+
+    def coordinates
+      client = GeocoderService::Client.new
+      @coordinates ||= client.geocode(@ad[:city])
     end
   end
 end
